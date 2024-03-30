d7a7839df69c511a2cf61d71aea4fae5adea67c6
Jonnthan3visan <joptrabajo@hotmail.com>
2024-03-30 02:30:26 -0300

    resuelto

---
+++ j2.js
@@ -0,0 +1,29 @@
+const leer = require ("prompt-sync")();
+
+let secreto ;
+let maxii=6 ; 
+let minimo= 0;
+let elecionUsuario = 0;
+secreto= Math.round(Math.random()*(minimo-maxii)+maxii)
+console.log("Adivina el numero secreto en solo 3 intentos");
+console.log("empezemos eligendo u numero");
+
+
+for (i = 1; i < 4 ; i++) {
+   
+    elecionUsuario =leer ();
+
+    if (elecionUsuario == secreto ) {       
+       console.log("ganaste");
+        i = (6);
+      
+     }else {
+
+        console.log("intento numero",i);
+        
+    }
+    
+}
+
+
+
